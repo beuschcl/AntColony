2007dc078b38aa11da1c96099b65b68d294ed81b
beuschcl <beuschcl+github@gmail.com>
2026-07-25 15:47:18 -0500

    Establish Python package structure

---
+++ src/ant_colony/__init__.py
@@ -0,0 +1,1 @@
+"""Deterministic environment simulation for the Ant Colony project."""
---
+++ tests/test_package.py
@@ -0,0 +1,4 @@
+def test_ant_colony_package_can_be_imported() -> None:
+    import ant_colony
+
+    assert ant_colony.__name__ == "ant_colony"
